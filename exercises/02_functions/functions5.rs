671302d7ef87901a37d93476a4e27bb127a39ce9
tv-phuong <tv.phuong.93@gmail.com>
2024-06-12 21:53:10 +0200

    Day1 Exo getting stronger

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -6,10 +6,10 @@
 // I AM NOT DONE
 
 fn main() {
-    let answer = square(3);
+    let answer: i32= square(3);
     println!("The square of 3 is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -8,7 +8,7 @@
 // I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a: [i32;100] = [3;100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
